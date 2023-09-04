a781c467249c4838e621cfa52a6cc739787418b0
LeeChoongHwan <choo901@naver.com>
2023-09-04 14:23:08 +0900

    [FIX] : 🐛  ContactManager 주석 추가

--- app/src/main/java/com/example/myapplication/data/ContactManager.kt
+++ app/src/main/java/com/example/myapplication/data/ContactManager.kt
@@ -3,10 +3,12 @@ package com.example.myapplication.data
 object ContactManager {
     private val contactList = mutableListOf<Contact>()
 
+    //전화번호 추가
     fun addContact(contact: Contact) {
         contactList.add(contact)
     }
 
+    //전화번호 업데이트
     fun updateContact(contact: Contact) {
         val existingContact = findContactByPhone(contact.phone)
         if (existingContact != null) {
@@ -15,6 +17,7 @@ object ContactManager {
         }
     }
 
+    //전화번호 삭제
     fun deleteContactById(phone: String) {
         val contactToRemove = findContactByPhone(phone)
         if (contactToRemove != null) {
@@ -22,6 +25,7 @@ object ContactManager {
         }
     }
 
+    //전화번호 입력 시 Contact return
     fun findContactByPhone(phone: String): Contact? {
         return contactList.find { it.phone == phone }
     }
