6a1243058f3f847b6ce0081e42d0fd7690ac0c95
Seulbi-Lee-project <ifssfws@gmail.com>
2023-09-04 16:00:16 +0900

    [FEAT] : ⚙️ 디데일 페이지 뷰바인딩

--- app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
+++ app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
@@ -5,17 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.FragmentContactDetailBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var binding:FragmentContactDetailBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -34,18 +33,17 @@ class ContactDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_detail, container, false)
+        binding = FragmentContactDetailBinding.inflate(layoutInflater)
+        binding.messageBtnDetail.setOnClickListener {
+            Toast.makeText(context, "message", Toast.LENGTH_SHORT).show()
+        }
+        binding.callBtnDetail.setOnClickListener {
+            Toast.makeText(context, "call", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactDetailFragment.
-         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
