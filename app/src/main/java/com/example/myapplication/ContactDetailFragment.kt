40d4d3dff8424cdb7f6bf4e0a1a1ce5a96143037
Seulbi-Lee-project <ifssfws@gmail.com>
2023-09-04 16:04:04 +0900

    [REFACTOR] : ♻️ 불필요한 부분 삭제

--- app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
+++ app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
@@ -6,26 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.databinding.FragmentContactDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 private lateinit var binding:FragmentContactDetailBinding
 
 class ContactDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -41,17 +29,5 @@ class ContactDetailFragment : Fragment() {
             Toast.makeText(context, "call", Toast.LENGTH_SHORT).show()
         }
         return binding.root
-    }
-
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ContactDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
