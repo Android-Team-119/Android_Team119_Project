ed8ff523c4b39e1462d055729fac6fd4fdd30c9b
LeeChoongHwan <choo901@naver.com>
2023-09-04 17:12:28 +0900

    [FEAT] : ⚙️ 전화, 문자 페이지 이동 구현

--- app/src/main/java/com/example/myapplication/viewpaperadapter/ViewPagerFragmentAdapter.kt
+++ app/src/main/java/com/example/myapplication/viewpaperadapter/ViewPagerFragmentAdapter.kt
@@ -3,13 +3,14 @@ package com.example.myapplication.viewpaperadapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.myapplication.ContactDetailFragment
 import com.example.myapplication.testfragment.Test1Fragment
 import com.example.myapplication.testfragment.Test2Fragment
 
 class ViewPagerFragmentAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
 
     // 1. ViewPager2에 연결할 Fragment 들을 생성
-    val fragmentList = listOf<Fragment>(Test1Fragment(), Test2Fragment())
+    val fragmentList = listOf<Fragment>(Test1Fragment(), ContactDetailFragment())
 
     // 2. ViesPager2에서 노출시킬 Fragment 의 갯수 설정
     override fun getItemCount(): Int {
