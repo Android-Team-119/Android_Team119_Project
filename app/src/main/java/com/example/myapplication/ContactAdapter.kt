312d52dfd27b34e456e75087f3ef333460eb9ffa
LeeChoongHwan <choo901@naver.com>
2023-09-04 22:27:17 +0900

    [FEAT] : ⚙️ Adapter 기능 구현

--- app/src/main/java/com/example/myapplication/ContactAdapter.kt
+++ app/src/main/java/com/example/myapplication/ContactAdapter.kt
@@ -1,14 +1,19 @@
 package com.example.myapplication
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.data.Contact
+import com.example.myapplication.data.ContactManager
 import com.example.myapplication.databinding.ContactlistItemBinding
 
-class ContactAdapter(private val contactList: List<Contact>) :
+class ContactAdapter() :
     RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+
+    private val contactList = ContactManager.getContactList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ContactlistItemBinding.inflate(
@@ -21,6 +26,21 @@ class ContactAdapter(private val contactList: List<Contact>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val contact = contactList[position]
         holder.bind(contact)
+        holder.itemView.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putParcelable("selectedContact",contact)
+
+            val contactDetailFragment = ContactDetailFragment()
+
+            contactDetailFragment.arguments = bundle
+
+            // Fragment 전환
+            val fragmentManager = (it.context as AppCompatActivity).supportFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(R.id.frag_container, contactDetailFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     override fun getItemCount(): Int {
