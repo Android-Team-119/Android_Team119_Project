8b4da529dbbff657fc377184b573d76b0a3ae7ec
Seulbi-Lee-project <ifssfws@gmail.com>
2023-09-06 17:41:05 +0900

    [FEAT] : ⚙️ 연락처 연결

--- app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
+++ app/src/main/java/com/example/myapplication/ContactDetailFragment.kt
@@ -3,27 +3,33 @@ package com.example.myapplication
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Context.NOTIFICATION_SERVICE
-
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplication.data.Contact
+import com.example.myapplication.data.ContactManager
 import com.example.myapplication.databinding.FragmentContactDetailBinding
-
-
 
 @Suppress("DEPRECATION")
 class ContactDetailFragment : Fragment() {
@@ -44,9 +50,51 @@ class ContactDetailFragment : Fragment() {
         binding.messageBtnDetail.setOnClickListener {
           Toast.makeText(context, "message", Toast.LENGTH_SHORT).show()
         }
+        // 퍼미션 허용했는지 확인
+        val status = ContextCompat.checkSelfPermission(requireContext(), "android.permission.READ_CONTACTS")
+        if (status == PackageManager.PERMISSION_GRANTED) {
+            Log.d("test", "permission granted")
+        } else {
+            // 퍼미션 요청 다이얼로그 표시
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf<String>("android.permission.READ_CONTACTS"), 100)
+            Log.d("test", "permission denied")
+        }
+
+        // ActivityResultLauncher 초기화, 결과 콜백 정의
+        requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == AppCompatActivity.RESULT_OK) {
+                Log.d("test", "Uri : ${it.data!!.data!!}")
+                var contentResolver:ContentResolver = requireContext().contentResolver
+                val cursor = contentResolver.query(
+                    it.data!!.data!!,
+                    arrayOf<String>(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Phone.NUMBER,
+                        // ContactsContract.Contacts.PHOTO_URI,
+                        // ContactsContract.Contacts._ID,
+                    ),
+                    null,
+                    null,
+                    null
+                )
+                Log.d("test", "cursor size : ${cursor?.count}")
+
+                if (cursor!!.moveToFirst()) {
+                    val name = cursor.getString(0)
+                    val phone = cursor.getString(1)
+                    // val photoURI = cursor.getString(2)
+                    // val id = cursor.getString(3)
+                    Toast.makeText(requireContext(), "$name, $phone", Toast.LENGTH_SHORT).show()
+                    var content = Contact(null, name, phone, "없음", false)
+                    ContactManager.addContact(content)
+                    ContactAdapter().addcontact(content)
+                }
+            }
+        }
 
         binding.phoneBookDetail.setOnClickListener {
-
+            val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+            requestLauncher.launch(intent)
         }
 
         val contact = Contact(null, "디폴트 네임", "010-0000-0000", "email@email.com", false)
@@ -131,5 +179,19 @@ class ContactDetailFragment : Fragment() {
             addAction(R.mipmap.ic_launcher,"Action",pendingIntent)
         }
         manager.notify(11,builder.build())
+    }
+
+    // 다이얼로그에서 퍼미션 허용했는지 확인
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d("test", "permission granted")
+        } else {
+            Log.d("test", "permission denied")
+        }
     }
 }
